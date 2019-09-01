57cafb1c5841cf1602ff38e503fde15f022962d2
Saeid Amanzadeh <saeid.amanzadehgharehtappeh@pearson.com>
2019-09-01 10:32:49 -0500

    SeasonDisplay css

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,20 +1,36 @@
+import "./SeasonDisplay.css";
 import React from "react";
-
+const seasonConfg = {
+    summer: {
+        text: "Lets hit the beach!",
+        iconName: "sun"
+    },
+    winter: {
+        text: "Burr, It's chill!",
+        iconName: "snowflake"
+    }
+};
 const getSeason = (lat, month) => {
     if(month > 2 && month < 9){
-        return lat > 0 ? "Summer" : "Winter";
+        return lat > 0 ? "summer" : "winter";
     }else {
-        return lat > 0 ? "Winter" : "Summer";
+        return lat > 0 ? "winter" : "summer";
     }
 };
 
 const SeasonDisplay = (props) => {
     const season = getSeason(props.lat, new Date().getMonth());
-
+    const {text, iconName } = seasonConfg[season];
     console.log(season);
 
     return (
-      <div>Season Display!</div>
+      <div className={`season-display ${season}`}>
+          <i className={`icon-left massive ${iconName} icon`}></i>
+          <h1>
+          {text}
+          </h1>
+          <i className={`icon-right massive ${iconName}  icon`}></i>
+      </div>
     );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 class App extends React.Component {
     state = { lat: null , errorMessage: ''};
@@ -10,9 +11,9 @@ class App extends React.Component {
                 this.setState({ lat: position.coords.latitude }),
             err => this.setState({ errorMessage: err.message })
         );
-    }
+    };
 
-    render() {
+    renderContent() {
         if(this.state.lat){
             return <SeasonDisplay lat={this.state.lat} />;
 
@@ -21,7 +22,15 @@ class App extends React.Component {
             return <div> Err: { this.state.errorMessage } </div>;
         };
 
-        return <div>Loading...</div>;
+        return <Spinner message="Please accept location request!"/>
+    };
+
+    render() {
+        return(
+            <div className="border red">
+                {this.renderContent()}
+            </div>
+        );
     };
 };
 
